4cfdc3ac6227e17928deaade425b6afedc788350
Choi Jeong Il <3000mn@tukorea.ac.kr>
2023-11-03 23:50:50 +0900

    ClientStructs 작성

---
+++ TermProject_Final/ClientStructs.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "stdafx.h"
+
+struct PlayerData {
+	Type type;
+	Type subType;
+	Vector2 pos;
+};
+
+struct EnemyData {
+	EnemyType enemyType;
+	Vector2 pos;
+};
+
+struct BulletData {
+	BulletType enemyType;
+	Vector2 pos;
+	Vector2 dir;
+};
