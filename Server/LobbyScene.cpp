81ecdf2e68c4b285a37ee9905690d66601457fbe
SaintPingu <45813433+SaintPingu@users.noreply.github.com>
2023-11-14 11:45:30 +0900

    add LobbyScene ProcessCommand()

--- Server/LobbyScene.cpp
+++ Server/LobbyScene.cpp
@@ -2,6 +2,17 @@
 #include "LobbyScene.h"
 
 
+
+void LobbyPlayer::Move()
+{
+	if (!isMoving) {
+		return;
+	}
+
+}
+
+
+
 void LobbyScene::Init()
 {
 
@@ -9,21 +20,71 @@ void LobbyScene::Init()
 
 void LobbyScene::ProcessCommand(int clientID, Command command, void* data)
 {
+	assert(players.count(clientID));
+	auto& player = players[clientID];
+
 	ClientLobbyCmd clientCmd = (ClientLobbyCmd)command;
 
 	switch (clientCmd) {
 	case ClientLobbyCmd::Terminate:
-		break;
+		// 해당 클라이언트와 연결 종료 코드 추가 필요.
+		players.erase(clientID);
+		return;
 	case ClientLobbyCmd::MoveLeft:
+		if (player->dir == Dir::Right) {
+			player->dir = Dir::Empty;
+			player->isMoving = false;
+		}
+		else {
+			player->dir = Dir::Left;
+			player->isMoving = true;
+		}
+
 		break;
 	case ClientLobbyCmd::MoveRight:
+		if (player->dir == Dir::Left) {
+			player->dir = Dir::Empty;
+			player->isMoving = false;
+		}
+		else {
+			player->dir = Dir::Right;
+			player->isMoving = true;
+		}
+
 		break;
 	case ClientLobbyCmd::MoveUp:
+		if (player->dir == Dir::Down) {
+			player->dir = Dir::Empty;
+			player->isMoving = false;
+		}
+		else {
+			player->dir = Dir::Up;
+			player->isMoving = true;
+		}
+
 		break;
 	case ClientLobbyCmd::MoveDown:
+		if (player->dir == Dir::Up) {
+			player->dir = Dir::Empty;
+			player->isMoving = false;
+		}
+		else {
+			player->dir = Dir::Down;
+			player->isMoving = true;
+		}
+
 		break;
 	default:
 		assert(0);
 		break;
 	}
 }
+
+// 하나의 키 입력으로 지속적으로 플레이어를 움직여야 한다.
+void LobbyScene::Update()
+{
+	for (auto& [clientID, player] : players) {
+		player->Move();
+	}
+}
+
--- Server/LobbyScene.h
+++ Server/LobbyScene.h
@@ -2,19 +2,22 @@
 #include "Scene.h"
 
 class LobbyPlayer {
-	POINT aboutMapPos;
-	POINT pos = { 0, };
-	RECT rectDraw = { 0, };
-	RECT rectImage = { 0, };
-	Dir dir = Dir::Down;
+public:
+	POINT pos = { 60, 232 };
+	POINT aboutMapPos = { pos.x, pos.y };
+	RECT rect = { 0, };
+	Dir dir = Dir::Empty;
 	bool isMoving = false;
+
+	void Move();
 };
 
 class LobbyScene : public Scene {
 private:
-	std::unordered_map<int, LobbyPlayer> players{};
+	std::unordered_map<int, std::shared_ptr<LobbyPlayer>> players{};
 
 public:
 	virtual void Init() override;
 	virtual void ProcessCommand(int clientID, Command command, void* data) override;
+	void Update();
 };
